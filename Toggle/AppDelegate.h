98240cf7d74702c45c51961a318062fd83f6099a
Aditya Pathak <Aditya@Adityas-Mac-mini.local>
2017-01-02 15:17:37 +0530

    Initial Commit

---
+++ Toggle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Toggle
+//
+//  Created by Aditya on 02/01/17.
+//  Copyright © 2017 AP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
